cf58e6bfdf9c8621dbb0104fea0d6fcb82b4dcbd
Finn Beer <finn.beer@gmail.com>
2020-12-20 01:23:56 +0100

    new structure

--- network_backend/src/resolvers/role.resolver.ts
+++ network_backend/src/resolvers/role.resolver.ts
@@ -1,33 +1,31 @@
-import { Resolver, Query, Ctx, Arg, Mutation, Authorized, Info } from 'type-graphql';
-
-import { MyContext } from '../utils/interfaces/context.interface';
+import { Arg, Authorized, Mutation, Query, Resolver } from 'type-graphql';
+import { getRepository } from 'typeorm';
 import { Role } from '../entity/role.entity';
 import { User } from '../entity/user.entity';
-import { getRepository } from 'typeorm';
-import { RoleValidator } from "../validators/role.validator"
+import { RoleValidator } from '../validators/role.validator';
 
 @Resolver(() => Role)
 export class RoleResolver {
   @Authorized('ADMIN')
   @Query(() => [Role])
-  public async getRoles(@Ctx() ctx: MyContext): Promise<Role[]> {
+  public async getRoles(): Promise<Role[]> {
     return await getRepository(Role).find();
   }
 
   @Authorized()
   @Mutation(() => Role)
-  public async addRole(@Arg('input') input: RoleValidator, @Ctx() ctx: MyContext): Promise<Role> {
+  public async addRole(@Arg('input') input: RoleValidator): Promise<Role> {
     const role = new Role(input);
-    await getRepository(Role).save(role)
+    await getRepository(Role).save(role);
     return role;
   }
 
   @Authorized()
   @Mutation(() => Boolean)
-  public async removeRole(@Arg('name') name: string, @Ctx() ctx: MyContext): Promise<boolean> {
+  public async removeRole(@Arg('name') name: string): Promise<boolean> {
     const role = await getRepository(Role).findOne({ name });
     try {
-      await getRepository(Role).remove(role)
+      await getRepository(Role).remove(role);
     } catch (err) {
       console.error(err);
       throw new Error(err);
@@ -37,14 +35,10 @@ export class RoleResolver {
 
   @Authorized()
   @Mutation(() => User)
-  public async assignRoleToUser(
-    @Arg('roleName') roleName: string,
-    @Arg('email') email: string,
-    @Ctx() ctx: MyContext,
-  ): Promise<User | null> {
+  public async assignRoleToUser(@Arg('roleName') roleName: string, @Arg('email') email: string): Promise<User | null> {
     const role = await getRepository(Role).findOne({ name: roleName });
     if (!role) throw new Error('Role not found!');
-    const user = await getRepository(User).findOne({relations: ["roles"], where:{ email }});
+    const user = await getRepository(User).findOne({ relations: ['roles'], where: { email } });
     if (!user) throw new Error('User not found!');
     user.roles.push(role);
     await getRepository(User).save(user);
@@ -56,16 +50,15 @@ export class RoleResolver {
   public async removeRoleFromUser(
     @Arg('roleName') roleName: string,
     @Arg('email') email: string,
-    @Ctx() ctx: MyContext,
   ): Promise<User | null> {
     const role = await getRepository(Role).findOne({ name: roleName });
     if (!role) throw new Error('Role not found!');
-    const user = await getRepository(User).findOne({relations: ["roles"], where:{ email }});
+    const user = await getRepository(User).findOne({ relations: ['roles'], where: { email } });
     if (!user) throw new Error('User not found!');
-    user.roles = user.roles.filter(r => {
-      r.id !== role.id
-  })
-    await getRepository(User).save(user)
+    user.roles = user.roles.filter((r) => {
+      r.id !== role.id;
+    });
+    await getRepository(User).save(user);
     return user;
   }
 }

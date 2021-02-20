d223d2e26e0d956c052eb955df44682236118016
HeyImNoxz <finn.beer@gmail.com>
2021-02-20 15:01:43 +0100

    fixed some bugs

--- network_frontend/vite.config.ts
+++ network_frontend/vite.config.ts
@@ -1,13 +1,16 @@
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import WindiCSS from 'vite-plugin-windicss';
 const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
   server: {
     port: 8080,
   },
-  plugins: [vue(), ...WindiCSS()],
+  plugins: [vue(), ...WindiCSS(), VitePWA()],
 });

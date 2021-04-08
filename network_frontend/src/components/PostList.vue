7d1495c17575be295c8746f88462b4c1bf537f46
Finn Beer <finn.beer@gmail.com>
2021-04-08 18:17:01 +0200

    updated text

--- network_frontend/src/components/PostList.vue
+++ network_frontend/src/components/PostList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full h-full flex items-center flex-col pb-14">
-    <div class="dark:text-gray-50 text-gray-900 mt-20 font-semibold text-xl" v-if="posts?.length === 0">
+    <div class="dark:text-gray-50 text-gray-900 mt-20 font-semibold text-xl" v-if="posts?.length === 0 && posts">
       {{ emptyText }}
     </div>
     <post-card v-for="post in posts" :key="post.id" :post="post" />

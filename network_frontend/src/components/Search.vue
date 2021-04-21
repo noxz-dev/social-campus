2508843a73504dcf66ccd099cf2a058acda591b1
Finn Beer <finn.beer@gmail.com>
2021-04-21 16:28:12 +0200

    fixed style search bug

--- network_frontend/src/components/Search.vue
+++ network_frontend/src/components/Search.vue
@@ -4,11 +4,16 @@
       @click="isFocus = true"
       v-model="searchString"
       :iconPadding="2"
-      inputClasses="text-sm md:text-md pl-10"
+      inputClasses="text-sm md:text-md pl-10 !pr-0"
       placeholder="Suche nach Usern, Tags oder Gruppen"
     >
       <template v-slot:icon>
-        <svg class="h-6 w-6 dark:fill-white fill-dark700" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg
+          class="h-6 w-6 dark:fill-white fill-dark700"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <path
             fill-rule="evenodd"
             clip-rule="evenodd"

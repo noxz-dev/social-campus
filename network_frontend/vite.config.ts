60b2a327ec984f9a70258da262c51c991a8d954b
Finn Beer <finn.beer@gmail.com>
2021-06-21 00:12:54 +0200

    updated icons

--- network_frontend/vite.config.ts
+++ network_frontend/vite.config.ts
@@ -29,17 +29,17 @@ export default defineConfig({
         background_color: '#191a20',
         icons: [
           {
-            src: '/pwa-192x192.png',
+            src: '/campus_192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/campus_512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/campus_512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
